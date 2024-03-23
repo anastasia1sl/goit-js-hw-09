4b6a9fa80948689270209f5d7572d4a63a866c17
anastasia1sl <slobodyanik.a97@gmail.com>
2024-03-23 12:56:15 +0200

    hm final fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
-const emailInput = document.querySelector('input');
-const textarea = document.querySelector('textarea');
+const emailInput = document.querySelector('[name="email"]');
+const textarea = document.querySelector('[name="message"]');
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', collectFormData);
@@ -14,7 +14,9 @@ function onFormSubmit(event) {
   const message = textarea.value.trim();
 
   if (email === '' || message === '') {
-    alert('The fields can not be empty');
+    alert(
+      'The Please fill in all fields before submitting the form can not be empty'
+    );
     return;
   } else {
     console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
@@ -37,7 +39,7 @@ function collectFormData() {
 function storedData() {
   const savedMessage = localStorage.getItem('feedback-form-state');
 
-  if (savedMessage !== null && savedMessage !== 'undefined') {
+  if (savedMessage !== null && savedMessage !== undefined) {
     const { email, message } = JSON.parse(savedMessage);
     emailInput.value = email;
     textarea.value = message;
